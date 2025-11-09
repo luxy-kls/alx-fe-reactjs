96d45017cfdb7d850f604499f655bbd5e16f0ef6
luxy-kls <sackeykeyman@gmail.com>
2025-11-09 19:55:31 +0000

    update UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,17 @@
-function UserProfile(props) {
+import React, { useContext } from 'react';
+import UserContext from './components/UserContext';
+
+
+function UserProfile() {
+    const userData =useContext(UserContext);
+    
     return(
         <>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
+        <h2>{userData.name}</h2>
+        <p>Age: {userData.age}</p>
+        <p>Bio: {userData.bio}</p>
         </>
     );
 };
 
-export default UserProfile
+export default UserProfile;
