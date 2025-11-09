894dcc41c76a3323a6a7635db3630555d75b7081
luxy-kls <sackeykeyman@gmail.com>
2025-11-09 15:52:42 +0000

    Fixed bug

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
-import Counter from './components/Counter ';
+import Counter from './components/Counter';
 
 function App() {
   
