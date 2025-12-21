b263779bc94746753d729f22eeb0a2c736030963
luxy-kls <sackeykeyman@gmail.com>
2025-12-21 19:48:35 +0000

    Added authentication

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,12 +1,19 @@
 import { Navigate } from 'react-router-dom'
 
-const isAuthenticated = false // change to true to allow access
+export function useAuth() {
+  // Simulated authentication state
+  const isAuthenticated = false // change to true to allow access
+
+  return { isAuthenticated }
+}
 
 function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth()
+  
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />
   }
-
+  
   return children
 }
 
