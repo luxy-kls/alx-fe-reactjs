15c4cc3a203b68d323ea4fdbdf9d9753025feff8
* <sackeykeyman@gmail.com>
2025-12-21 18:46:46 +0000

    Updated imports in  main.jsx and PostComponents.jsx

--- react-query-demo/src/components/PostsComponents.jsx
+++ react-query-demo/src/components/PostsComponents.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from 'react-query'
-
+import { useQuery } from '@tanstack/react-query'
 const fetchPosts = async () => {
   const response = await fetch('https://jsonplaceholder.typicode.com/posts')
 
--- react-query-demo/src/main.jsx
+++ react-query-demo/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
